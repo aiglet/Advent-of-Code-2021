10269d4d3a495bb1ba3487ad246c88b495edd75f
aiglet <aiglet@gmail.com>
2021-12-01 12:57:52 -0500

    * Update code to eliminate linter errors * Add comments

--- Day 01/1-2.py
+++ Day 01/1-2.py
@@ -5,23 +5,33 @@ lines = data.readlines()
 
 # PART TWO
 
-def sonar_average(lines):
-# Set up a counter
+
+def sonar_average(input_file):
+    # Set up a counter
     counter = 0
-    sum1 = (int(lines[0]) + int(lines[1]) + int(lines[2]))
 
-    for i in range (len(lines) - 2):
-        line1 = int(lines[i])
-        line2 = int(lines[i + 1])
-        line3 = int(lines[i + 2])
+    # Establish the first sum to start with
+    sum1 = (int(input_file[0]) + int(input_file[1]) + int(input_file[2]))
 
-        sum2 = line1 + line2 + line3
+    # Iterate through the file and select each group of three
+    for i in range(len(input_file) - 2):
+        line1 = int(input_file[i])
+        line2 = int(input_file[i + 1])
+        line3 = int(input_file[i + 2])
 
+        # Sum each group
+        sum2: int = line1 + line2 + line3
+
+        # Compare the new group to the old one and increment the counter as appropriate
         if sum2 > sum1:
             counter += 1
 
+        # Reset sum1 to the new value
         sum1 = sum2
 
+    # Print the output
     print(f"The count is {counter}.")
 
+
+# Run the function
 sonar_average(lines)
