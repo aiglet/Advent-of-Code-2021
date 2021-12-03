605f583fcc20003d9a18e6da9da643c971f3dd31
aiglet <aiglet@gmail.com>
2021-12-03 15:50:07 -0500

    This is... closer to working?  The iteration's still broken.

--- Day 03/3-1.py
+++ Day 03/3-1.py
@@ -9,25 +9,24 @@ def calc_freq(data):
     print(freq)
 
 def power_consumption(data):
-    count0 = 0
-    count1 = 0
+
 
     gamma = ''
     epsilon = ''
+
 
     for line in data:
         linepos = 0
 
         while linepos < 5:
 
-            if line[linepos] == 0:
+            count0 = 0
+            count1 = 0
+
+            if line[linepos] == '0':
                 count0 += 1
-                print(f"{line[linepos]} is 0.")
-                print(count0)
-            elif line[linepos] == 1:
+            elif line[linepos] == '1':
                 count1 += 1
-                print(f"{line[linepos]} is 1.")
-                print(count1)
 
             if count0 > count1:
                 gamma += '0'
@@ -36,10 +35,10 @@ def power_consumption(data):
                 gamma += '1'
                 epsilon += '0'
 
-            print(f"Gamma is {gamma}.")
-            print(f"Epsilon is {epsilon}.")
-
             linepos += 1
+
+        print(f"Gamma is {gamma}.")
+        print(f"Epsilon is {epsilon}.")
 
 
 
