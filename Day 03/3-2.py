b1ada6fac3ebd3a4e9443e0310325f2024f66af5
aiglet <aiglet@gmail.com>
2021-12-06 17:30:51 -0500

    Okay. find_common does what it's supposed to. Now I just need to write the *rest* of the code... ;)

--- Day 03/3-2.py
+++ Day 03/3-2.py
@@ -3,51 +3,25 @@ with open("test.txt", "r") as f:
     lines = [x.strip() for x in f.readlines()]
 
 
-def find_common(data):
+def find_common(data, index):
 
     count0 = 0
     count1 = 0
-    index = 0
 
     for line in data:
 
         line = list(line)
 
-        while index < len(line[0]):
-            if line[index] == '0':
-                count0 += 1
-                index += 1
-            elif line[index] == '1':
-                count1 += 1
-                index += 1
+        if line[index] == '0':
+            count0 += 1
+        elif line[index] == '1':
+            count1 += 1
 
     if count0 > count1:
-        return('0')
+        print(f"There are more 0s than 1s, the 0 count is {count0}, the 1 count is {count1}.")
     else:
-        return('1')
-
-
-def gasses(data):
-
-    oxy = []
-    C02 = []
-    index = 0
-
-    for item in data:
-        print(f"The current item is {item}.")
-        print(f"The current index is {index}.")
-        print(f"The common number is {find_common(data)}.")
-
-        if item[index] == find_common(data):
-            oxy += item
-        else:
-            C02 += item
-
-        index += 1
-
-    print(f"The oxy candidates are {oxy}.")
-    print(f"The C02 candidates are {C02}.")
+        print(f"There are more 1s than 0s, the 0 count is {count0}, the 1 count is {count1}.")
 
 
 
-gasses(lines)
+find_common(lines, 0)
